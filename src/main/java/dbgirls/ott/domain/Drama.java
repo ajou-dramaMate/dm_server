4ec488bd9d841ae1615e584f29d89b87bcb20656
munyeong0103 <jmuny@jeongmun-yeong-ui-MacBookAir.local>
2024-05-09 02:13:54 +0900

    feat : add domain structure (#1)

--- src/main/java/dbgirls/ott/domain/Drama.java
+++ src/main/java/dbgirls/ott/domain/Drama.java
@@ -1,4 +1,55 @@
 package dbgirls.ott.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "drama")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Drama {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "drama_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String member;
+
+    @Column(nullable = false)
+    private String summary;
+
+    @Column(nullable = false)
+    private Integer date;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column(nullable = false)
+    private String information;
+
+    @Column(nullable = false)
+    private Genre genre;
+
+    @Lob
+    @Column(nullable = false, length = 1000)
+    private byte[] image;
+
+    @OneToMany(mappedBy = "drama", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private final Set<OttDramaRelation> ottDramaRelations = new HashSet<>();
+
+    @OneToMany(mappedBy = "drama", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private final Set<Review> reviews = new HashSet<>();
 }
--- src/main/java/dbgirls/ott/domain/User.java
+++ src/main/java/dbgirls/ott/domain/User.java
@@ -1,5 +1,38 @@
 package dbgirls.ott.domain;
 
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String pw;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private final Set<Review> reviews = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private final Set<Community> communities = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private final Set<Comment> comments = new HashSet<>();
 }
